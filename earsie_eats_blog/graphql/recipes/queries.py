160e7aa2ffcab66be5c38048a58557483ffb1a03
Michael A. Gonzalez <genomics.geek.04.22@gmail.com>
2018-10-14 10:48:27 -0400

    Uprotected Routes so viewers could still read the blog without having to  login/register

--- earsie_eats_blog/graphql/recipes/queries.py
+++ earsie_eats_blog/graphql/recipes/queries.py
@@ -1,25 +1,25 @@
 from graphene import Node
+from graphene_django.filter import DjangoFilterConnectionField
 
 from . import filters, types
-from ..fields import ProtectedConnectionField
 
 
 class RecipesQuery(object):
 
     recipe = Node.Field(types.RecipeNode)
-    all_recipes = ProtectedConnectionField(
+    all_recipes = DjangoFilterConnectionField(
         types.RecipeNode,
         filterset_class=filters.RecipeFilter,
     )
 
     ingredient = Node.Field(types.IngredientNode)
-    all_ingredients = ProtectedConnectionField(
+    all_ingredients = DjangoFilterConnectionField(
         types.IngredientNode,
         filterset_class=filters.IngredientFilter,
     )
 
     step = Node.Field(types.StepNode)
-    all_steps = ProtectedConnectionField(
+    all_steps = DjangoFilterConnectionField(
         types.StepNode,
         filterset_class=filters.StepFilter,
     )
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,26 +3,37 @@ import React from 'react'
 import Loadable from 'react-loadable'
 import { Route, Switch } from 'react-router-dom'
 
+import Menu from 'common/app-bar'
 import { AuthRoute } from 'common/auth'
 import { AsyncLoader } from 'common/loaders'
+
+import About from 'pages/about'
 import Home from 'pages/home'
 import Login from 'pages/login'
+import Recipes from 'pages/recipes'
 import Register from 'pages/registration'
 
 
-const Pages = Loadable({
-  loader: () => import('pages'),
+const PrivateRoutes = Loadable({
+  loader: () => import('private-routes'),
   loading: AsyncLoader
 })
 
 
 const Routes = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route path="/app" render={() => <AuthRoute protectedComponent={<Pages />} />} />
-    <Route exact path="/login/" component={Login} />
-    <Route path="/register" component={Register} />
-  </Switch>
+  <React.Fragment>
+    <Menu />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/app/" component={Home} />
+      <Route exact path="/app/home/" component={Home} />
+      <Route exact path="/app/about/" component={About} />
+      <Route path="/app/recipes/" component={Recipes} />
+      <Route exact path="/login/" component={Login} />
+      <Route path="/register/" component={Register} />
+      <Route path="/management/" render={() => <AuthRoute protectedComponent={<PrivateRoutes />} />} />
+    </Switch>
+  </React.Fragment>
 )
 
 
